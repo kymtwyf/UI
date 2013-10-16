318d78cb176f1212b7c0525cfa904fde3291d1df
owenxu10 <owenxu10@yahoo.com.cn>
2013-10-16 17:57:46 +0800

    new Dialog and setting function

--- WebContent/ui/whoiswhere.view.js
+++ WebContent/ui/whoiswhere.view.js
@@ -14,62 +14,13 @@ sap.ui.jsview("ui.whoiswhere", {
 	*/ 
 	createContent : function(oController) {
 		
-		var pieChart = sap.ui.view({id:"piechart", viewName:"ui.PieChart", type:sap.ui.core.mvc.ViewType.JS});
+		var oShell= new sap.m.Shell("myShell");
 		
+		var oApp = new sap.m.App("myApp");
 		
-		var button1 = new sap.m.Button('bt_showByTimes', {
-			type: sap.m.ButtonType.Default,
-			icon: "sap-icon://line-charts",
-			//text: "Times"
-		});
+		var t_cost = sap.m.Text({text:"81.199"} ); 			//total cost
 		
-
-		var button2 = new sap.m.Button('bt_showByCost', {
-			type: sap.m.ButtonType.Default,
-			icon: "sap-icon://lead",
-			//text: "Cost"
-		});
-		
-		var button3 = new sap.m.Button('bt_showbyChart', {
-			type: sap.m.ButtonType.Default,
-			icon: "sap-icon://pie-chart",
-			press:function() {				
-				panel1.removeAllContent();
-				panel1.addContent(bar);
-				panel1.addContent(pieChart);
-				}
-		});
-		
-		var button4 = new sap.m.Button('bt_showByTable', {
-			type: sap.m.ButtonType.Default,
-			icon: "sap-icon://table-chart",
-			//text: "Table"
-			press:function() {
-				panel1.removeAllContent();
-				panel1.addContent(bar);
-				panel1.addContent(new sap.m.Text({
-			     text: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua."
-			    }));
-				}
-		});
-		
-		var Segmented1= new sap.m.SegmentedButton('Segmented1', {
-			buttons: [button1, button2], 
-			selectedButton: button1
-		});
-
-		var Segmented2= new sap.m.SegmentedButton('Segmented2', {
-			buttons: [button3, button4], 
-			selectedButton: button3
-		});
-		
-		
-		Segmented2.addStyleClass('segCont');
-		
-		var t_cost = sap.m.Text({text:"81.199"} );
-		
-		var objectheader = new sap.m.ObjectHeader({
-		      //title : "Turbine K7",
+		var objectheader = new sap.m.ObjectHeader({			//header 
 		      number : t_cost.getText(),
 		      numberUnit : "€",
 		      firstStatus : new sap.m.ObjectStatus({
@@ -79,31 +30,73 @@ sap.ui.jsview("ui.whoiswhere", {
 		        })
 		  });
 		
-		var bar = new sap.m.Bar({
+		var pieChart = sap.ui.view({id:"piechart", viewName:"ui.PieChart", type:sap.ui.core.mvc.ViewType.JS});
+		//pie chart of data
+		
+		
+		
+		var button1 = new sap.m.Button('bt_showByTimes', {
+			type: sap.m.ButtonType.Default,
+			icon: "sap-icon://line-charts",
+																//times button 
+		});
+		
+
+		var button2 = new sap.m.Button('bt_showByCost', {
+			type: sap.m.ButtonType.Default,
+			icon: "sap-icon://lead",
+																//cost button
+		});
+		
+		var button3 = new sap.m.Button('bt_showbyChart', {		//chart button
+			type: sap.m.ButtonType.Default,
+			icon: "sap-icon://pie-chart",
+			press:function() {				
+				oVBox_page.removeAllItems();
+				oVBox_page.addItem(bar);
+				oVBox_page.addItem(pieChart);
+				}
+		});
+		
+		var button4 = new sap.m.Button('bt_showByTable', {		//table button
+			type: sap.m.ButtonType.Default,
+			icon: "sap-icon://table-chart",
+			//text: "Table"
+			press:function() {
+				oVBox_page.removeAllItems();
+				oVBox_page.addItem(bar);
+				oVBox_page.addItem(new sap.m.Text({
+			     text: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua."
+			    }));
+				
+				}
+		});
+		
+		
+		var Segmented1= new sap.m.SegmentedButton('Segmented1', {	//segment button 1
+			buttons: [button1, button2], 
+			selectedButton: button1
+		});
+
+		var Segmented2= new sap.m.SegmentedButton('Segmented2', {	//segment button 2
+			buttons: [button3, button4], 
+			selectedButton: button3
+		});
+		
+		var bar = new sap.m.Bar({		// bar of segment buttons
 			contentLeft:Segmented1,
 			contentRight:Segmented2,
 			translucent:true
 		});
 		
-		var oVBox_page = new sap.m.VBox("hbox_page", {
+		var oVBox_page = new sap.m.VBox("hbox_page", {		//contain bar and pie chart 
 			items:[
 					bar,
 					pieChart
 			]
 		});
 		
-		var page1 = new sap.m.Page("pageContent",{
-			showNavButton: false,
-			enableScrolling:false,
-			height:"100%",
-			//  width:"120%",
-			//  headerText: "Three Balloons and a Text",
-			showHeader:false,
-			content: [oVBox_page] 
-			});
-	
-
-		var tab = new sap.m.IconTabBar({
+		var tab = new sap.m.IconTabBar({	//icon tab  under the objectheader
 			items: [
 					new sap.m.IconTabFilter({
 						icon: "sap-icon://globe",
@@ -126,16 +119,56 @@ sap.ui.jsview("ui.whoiswhere", {
 						text: "Time"
 					})
 				],
-				content: [page1]
+				content: [oVBox_page]
 			});
+		
 		tab.setExpandable(false);
 		tab.addStyleClass("tab");
 		
-		var oShell= new sap.m.Shell("myShell");
+		var Form = new sap.ui.commons.form.SimpleForm({ //simple form in the dialog
+			  editable: true,
+			  content : [
+			   
+			    new sap.m.Label({
+			      text: 'Cost Limit'
+			    }),
+			    new sap.m.Input({
+			      type: sap.m.InputType.Number,
+			      placeholder: 'Enter Cost Limit ...'
+			    })
+			  ]
+			});
+				     
+		var stdDialog = new sap.m.Dialog();
+		stdDialog = new sap.m.Dialog({// create standard dialog 
+		  title: "Configuration",
+		  content: Form,
+		  
+		  leftButton: new sap.m.Button({
+		    text: "Ok",
+		    press: function () {
+		    	alert("ok");
+		    	stdDialog.close();
+		    }
+		  }),
+		  rightButton: new sap.m.Button({
+		    text: "Cancel",
+		    press: function () {
+		    	alert("cancel");
+		    	stdDialog.close();
+		    }
+		  }),
+		  afterClose: function (oEvent) {
+		    // if dialog is closed by pressing on one of the buttons in dialog, 
+		    // a history back needs to be called.
+		    if (oEvent.getParameter("origin")) {
+		      sap.ui.getCore().getEventBus().publish("nav", "back");
+		    }
+		  }
+		});
+
 		
-		var oApp = new sap.m.App("myApp");
-		
-		var oVBox1 = new sap.m.VBox("hbox1", {
+		var oVBox1 = new sap.m.VBox("hbox1", {	// shorten the mergin between the object header and tab
 			items:[
 			       objectheader,tab
 			]
@@ -155,7 +188,8 @@ sap.ui.jsview("ui.whoiswhere", {
 			contentRight: [new sap.m.Button({
 				icon: "sap-icon://settings",
 					press : function() {
-						
+						 sap.ui.getCore().getEventBus().publish("nav", "virtual");
+						    stdDialog.open();
 					}	
 				})]
 		});
