21b26bffe465ad3eaa43708ef74359b8fe0439f8
owenxu10 <owenxu10@yahoo.com.cn>
2013-10-24 15:54:52 +0800

    perfect drill down popover

---
+++ WebContent/ui/howcost.controller.js
@@ -0,0 +1,59 @@
+sap.ui.controller("ui.howcost", {
+
+/**
+* Called when a controller is instantiated and its View controls (if available) are already created.
+* Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
+* @memberOf view.howcost
+*/
+//	onInit: function() {
+//
+//	},
+
+/**
+* Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
+* (NOT before the first rendering! onInit() is used for that one!).
+* @memberOf view.howcost
+*/
+	onBeforeRendering: function() {
+		// jQuery.ajax({
+		// 	url:"http://ld9415.wdf.sap.corp:8002/mouse/project/odata/Query.xsodata/Query?$select=TRIP_TOTAL&$filter=MANDT%20eq%20'578'&$format=json",
+		// 	error:function(){
+		// 		jQuery.sap.require("sap.m.MessageToast");
+		// 		 sap.m.MessageToast.show("Some error occurred when querying, please check the network and try again");
+		// 	},
+		// 	success:function(data){
+		// 		console.log("total data "+data.d);
+		// 	}
+		// });
+		jQuery.ajax({
+			url:"http://ld9415.wdf.sap.corp:8002/mouse/project/odata/Test.xsodata/TRIP_DEST?$select=LANDTEXX,ONE&$filter=MANDT%20eq%20'002'&$format=json",
+			error:function(error){
+				jQuery.sap.require("sap.m.MessageToast");
+				 sap.m.MessageToast.show(error+"");
+			},
+			success:function(data){
+				console.log(data);
+				var model = new sap.ui.model.json.JSONModel(data.d);
+				sap.ui.getCore().setModel(model);
+			}
+		})
+	},
+
+/**
+* Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
+* This hook is the same one that SAPUI5 controls get after being rendered.
+* @memberOf view.howcost
+*/
+//	onAfterRendering: function() {
+//
+//	},
+
+/**
+* Called when the Controller is destroyed. Use this one to free resources and finalize activities.
+* @memberOf view.howcost
+*/
+//	onExit: function() {
+//
+//	}
+
+});
---
+++ WebContent/util/tools.js
@@ -0,0 +1,71 @@
+jQuery.sap.declare("util.tools");
+
+util.tools = {
+		_F_Toast:function(message,timeDuring){//用来显示一条toast消息
+			jQuery.sap.require("sap.m.MessageToast");
+			 sap.m.MessageToast.show(message,{
+			 	duration:timeDuring?timeDuring:3000
+			 });
+		},
+		_F_PrepareData:function(whichOne){
+			
+			switch(whichOne){
+			case 1:
+				if(!logic.data._D_COUNTRY_COUNT.content){
+					var url = util.queries.getQuery()
+				}
+				break;//_D_COUNTRY_COUNT
+			case 2:break;//_D_COUNTRY_COST
+			case 3:break;//_D_REASON_COUNT
+			case 4:break;//_D_REASON_COST
+			case 5:break;//_D_PEOPLE_COUNT
+			case 6:break;//_D_PEOPLE_COST
+			case 7:break;//_D_YEAR_COUNT
+			case 8:break;//_D_YEAR_COST
+			}
+		},
+		onChangeDataSource:function(newPath){
+			//1 save the old data
+
+			//2 prepare the new data
+			//2.1 search if the required data exists in the model.data["newPath"]
+			//2.2 if not they send another request to get the data
+
+			//3 show the new data
+			
+		},
+
+		//这些是用来存放cost limit在cookie里面的
+		getCookie:function(c_name){
+			if (document.cookie.length>0)
+			  {
+			  c_start=document.cookie.indexOf(c_name + "=");
+			  if (c_start!=-1)
+			    { 
+			    c_start=c_start + c_name.length+1 ;
+			    c_end=document.cookie.indexOf(";",c_start);
+			    if (c_end==-1) c_end=document.cookie.length;
+			    return unescape(document.cookie.substring(c_start,c_end));
+			    } 
+			  }
+			return "";
+		},
+		checkCookie:function(){
+			var c_name=this.getCookie('costlimit');
+			var costlimit= 50000;
+			if (c_name!=null && c_name!="")
+			{
+				costlimit=c_name;
+			}
+			return costlimit;
+		},
+		setCookie:function(value,expiredays){
+			var exdate=new Date();
+			exdate.setDate(exdate.getDate()+expiredays);
+			document.cookie="costlimit"+ "=" +escape(value)+
+			((expiredays==null) ? "" : ";expires="+exdate.toGMTString());
+		}
+		///在cookie 存放cost limit到此结束
+}
+var bus = sap.ui.getCore().getEventBus();
+bus.subscribe("data","onChangeDataSource",util.tools.onChangeDataSource,this);
